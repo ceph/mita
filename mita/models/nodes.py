c68e27ccad4d24ca41f4d0b82d07a8bd1977707b
Alfredo Deza <adeza@redhat.com>
2015-08-13 16:18:24 -0400

    add a second model object for labels in a node
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- mita/models/nodes.py
+++ mita/models/nodes.py
@@ -1,6 +1,6 @@
 import datetime
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy_utils import ScalarListType
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.orm.exc import DetachedInstanceError
 from mita.models import Base
 
@@ -15,17 +15,37 @@ class Node(Base):
     image_name = Column(String(128))
     size = Column(String(128))
 
-    labels = Column(ScalarListType())
-
-    def __init__(self, name, keyname, image_name, size):
+    def __init__(self, name, keyname, image_name, size, labels=None):
         self.name = name
         self.keyname = keyname
         self.image_name = image_name
         self.size = size
         self.created = datetime.datetime.utcnow()
+        if labels:
+            for l in labels:
+                Label(self, l)
 
     def __repr__(self):
         try:
             return '<Node %r>' % self.name
         except DetachedInstanceError:
             return '<Node detached>'
+
+
+class Label(Base):
+
+    __tablename__ = 'labels'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256), nullable=False, unique=True, index=True)
+    node_id = Column(Integer, ForeignKey('nodes.id'))
+    node = relationship('node', backref=backref('labels', lazy='dynamic'))
+
+    def __init__(self, node, name):
+        self.node = node
+        self.name = name
+
+    def __repr__(self):
+        try:
+            return '<Label %r>' % self.name
+        except DetachedInstanceError:
+            return '<Label detached>'
