1ef1316cb3b268a58a26b02749cf4c063671ae63
Alfredo Deza <adeza@redhat.com>
2016-01-11 13:24:11 -0500

    add logging config for tests
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- mita/tests/config.py
+++ mita/tests/config.py
@@ -13,6 +13,35 @@ app = {
     'debug': False,
 }
 
+logging = {
+    'root': {'level': 'INFO', 'handlers': ['console']},
+    'loggers': {
+        'mita': {'level': 'DEBUG', 'handlers': ['console']},
+        'pecan': {'level': 'DEBUG', 'handlers': ['console']},
+        'py.warnings': {'handlers': ['console']},
+        '__force_dict__': True
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'color'
+        }
+    },
+    'formatters': {
+        'simple': {
+            'format': ('%(asctime)s %(levelname)-5.5s [%(name)s]'
+                       '[%(threadName)s] %(message)s')
+        },
+        'color': {
+            '()': 'pecan.log.ColorFormatter',
+            'format': ('%(asctime)s [%(padded_color_levelname)s] [%(name)s]'
+                       '[%(threadName)s] %(message)s'),
+        '__force_dict__': True
+        }
+    }
+}
+
 nodes = {
     'precise-slave': {
         'script': dedent("""#!/bin/bash
