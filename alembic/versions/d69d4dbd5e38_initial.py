8cba8b0d5ec1637cd7a1abed7b566dcdd009b211
Alfredo Deza <adeza@redhat.com>
2016-01-22 10:09:59 -0500

    create the initial migrations
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

---
+++ alembic/versions/d69d4dbd5e38_initial.py
@@ -0,0 +1,24 @@
+"""initial
+
+Revision ID: d69d4dbd5e38
+Revises: 
+Create Date: 2016-01-22 10:06:15.770467
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'd69d4dbd5e38'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
