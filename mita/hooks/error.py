f13239533095b4b8d00537396493bbb25a639b15
Alfredo Deza <adeza@redhat.com>
2016-01-29 12:51:05 -0500

    give more information on error
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- mita/hooks/error.py
+++ mita/hooks/error.py
@@ -10,6 +10,16 @@ class CustomErrorHook(PecanHook):
     Only needed for prod environments where it looks like multi-worker servers
     will swallow exceptions. This will ensure a traceback is logged correctly.
     """
+    def get_controller(self, state):
+        '''
+        Retrieves the actual controller name from the application
+        Specific to Pecan (not available in the request object)
+        '''
+        return state.controller.__str__().split()[2]
 
     def on_error(self, state, exc):
-        log.exception('unhandled error by Mita')
+        path = state.request.path
+        controller = self.get_controller(state)
+
+        log.error('unhandled error by controller: %s', controller)
+        log.exception('request path %s', path)
