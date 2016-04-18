5597984878c3cf51213f8f83f3f4d09da660330e
Alfredo Deza <adeza@redhat.com>
2016-04-18 17:12:38 -0400

    log information about created or not created node
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- mita/providers/openstack.py
+++ mita/providers/openstack.py
@@ -58,6 +58,11 @@ def create_node(**kw):
         ex_userdata=kw['script'], ex_keyname=kw['keyname']
     )
 
+    if not new_node:
+        logger.error("provider could not create node with details: %s", str(kw))
+
+    logger.info("created node: %s", new_node)
+
     if storage:
         logger.info("Creating %sgb of storage for: %s", storage, name)
         new_volume = driver.create_volume(storage, name)
