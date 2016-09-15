d13cdc8c81a64077ad426987161c36a64b0d67a2
Alfredo Deza <adeza@redhat.com>
2016-09-15 14:00:49 +0530

    hooks: reduce the verbosity of the on_error hook for unhandled exceptions
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- mita/hooks/error.py
+++ mita/hooks/error.py
@@ -1,4 +1,5 @@
 import logging
+from webob.exc import WSGIHTTPException
 from pecan.hooks import PecanHook
 
 
@@ -18,6 +19,15 @@ class CustomErrorHook(PecanHook):
         return state.controller.__str__().split()[2]
 
     def on_error(self, state, exc):
+        if isinstance(exc, WSGIHTTPException):
+            if exc.code == 404:
+                log.error("Not Found: %s" % state.request.url)
+                return
+            # explicit redirect codes that should not be handled at all by this
+            # utility
+            elif exc.code in [300, 301, 302, 303, 304, 305, 306, 307, 308]:
+                return
+
         path = state.request.path
         controller = self.get_controller(state)
 
