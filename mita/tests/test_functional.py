af06f0e4218c1b59b698ec98c6cd4044d353963b
Alfredo Deza <adeza@redhat.com>
2017-07-05 10:49:06 -0400

    tests: add tests for openstack purge
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- mita/tests/test_functional.py
+++
@@ -1,9 +0,0 @@
-from mita.tests import FunctionalTest
-
-
-class TestRootController(FunctionalTest):
-
-    def test_get(self):
-        response = self.app.get('/')
-        assert response.status_int == 200
-
--- mita/tests/unit/test_providers.py
+++ mita/tests/unit/test_providers.py
@@ -9,7 +9,8 @@ class TestOpenStackProvider(object):
 
     def setup(self):
         self.fake_get_driver = Mock()
-        self.node = namedtuple('Node', 'name')
+        self.node = namedtuple('Node', ['name', 'state'])
+        self.node.destroy = Mock()
         self.fake_get_driver.return_value = self.fake_get_driver
 
     def test_destroy_node(self):
@@ -19,10 +20,20 @@ class TestOpenStackProvider(object):
             providers.openstack.destroy_node(name='foo')
 
     def test_destroy_node_api_fails(self):
-        self.fake_get_driver.list_nodes = Mock(return_value=[self.node(name='foo')])
+        self.fake_get_driver.list_nodes = Mock(return_value=[self.node(name='foo', state=1)])
         # a 0 return value will mean this node was not destroyed by the API
         self.fake_get_driver.destroy_node = Mock(return_value=0)
         providers.openstack.get_driver = self.fake_get_driver
         with pytest.raises(RuntimeError):
             providers.openstack.destroy_node(name='foo')
 
+    def test_purge_succeeds(self):
+        self.fake_get_driver.list_nodes = Mock(return_value=[self.node(name='foo', state=7)])
+        providers.openstack.get_driver = self.fake_get_driver
+        assert providers.openstack.purge() is True
+
+    def test_purge_finds_nothing(self):
+        self.fake_get_driver.list_nodes = Mock(return_value=[])
+        self.fake_get_driver.destroy_node = Mock(return_value=0)
+        providers.openstack.get_driver = self.fake_get_driver
+        assert providers.openstack.purge() is None
